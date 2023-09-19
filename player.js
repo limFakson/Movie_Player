446683b7b820fcef17c979b3fcc8e344cafdcb78
limFakson <fakeyejoshua2005@gmail.com>
2023-09-19 19:37:29 +0100

    commit

---
+++ player.js
@@ -0,0 +1,60 @@
+//checkboxes
+const checkboxOne = document.getElementById('checkbox-one');
+const checkboxTwo = document.getElementById('checkbox-two');
+const checkboxThree = document.getElementById('checkbox-three');
+const checkboxFour = document.getElementById('checkbox-four');
+const checkboxFive = document.getElementById('checkbox-five');
+
+//labels
+const labelOne = document.getElementById('label-one');
+const labelTwo = document.getElementById('label-two');
+const labelThree = document.getElementById('label-three');
+const labelFour = document.getElementById('label-four');
+const labelFive = document.getElementById('label-five');
+// Movie banner carousel changer i.e checkbox
+
+//label for checked checkbox
+labelOne.addEventListener('click', ()=>{
+    checkboxOne.checked = true;
+    checkboxTwo.checked = false;
+    checkboxThree.checked = false;
+    checkboxFour.checked = false;
+    checkboxFive.checked = false;
+});
+labelTwo.addEventListener('click', ()=>{
+    checkboxOne.checked = false;
+    checkboxTwo.checked = true;
+    checkboxThree.checked = false;
+    checkboxFour.checked = false;
+    checkboxFive.checked = false;
+});
+labelThree.addEventListener('click', ()=>{
+    checkboxOne.checked = false;
+    checkboxTwo.checked = false;
+    checkboxThree.checked = true;
+    checkboxFour.checked = false;
+    checkboxFive.checked = false;
+});
+labelFour.addEventListener('click', ()=>{
+    checkboxOne.checked = false;
+    checkboxTwo.checked = false;
+    checkboxThree.checked = false;
+    checkboxFour.checked = true;
+    checkboxFive.checked = false;
+});
+labelFive.addEventListener('click', ()=>{
+    checkboxOne.checked = false;
+    checkboxTwo.checked = false;
+    checkboxThree.checked = false;
+    checkboxFour.checked = false;
+    checkboxFive.checked = true;
+});
+
+//window preload sets
+window.addEventListener('load', ()=>{
+    checkboxOne.checked = false;
+    checkboxTwo.checked = false;
+    checkboxThree.checked = true;
+    checkboxFour.checked = false;
+    checkboxFive.checked = false;
+});
