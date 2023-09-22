d1444ac3aa38eea37309295bf914aa2fd6fcb344
limFakson <fakeyejoshua2005@gmail.com>
2023-09-22 09:43:36 +0100

    commit

--- carousel.js
+++ carousel.js
@@ -1,69 +1,8 @@
-//labels
-const labelOne = document.getElementById('label-one');
-const labelTwo = document.getElementById('label-two');
-const labelThree = document.getElementById('label-three');
-const labelFour = document.getElementById('label-four');
-const labelFive = document.getElementById('label-five');
-
-//checkboxes
-const checkboxOne = document.getElementById('checkbox-one');
-const checkboxTwo = document.getElementById('checkbox-two');
-const checkboxThree = document.getElementById('checkbox-three');
-const checkboxFour = document.getElementById('checkbox-four');
-const checkboxFive = document.getElementById('checkbox-five');
-
-// Movie banner carousel changer i.e checkbox
-
-//label for checked checkbox
-labelOne.addEventListener('click', ()=>{
-    checkboxOne.checked = true;
-    checkboxTwo.checked = false;
-    checkboxThree.checked = false;
-    checkboxFour.checked = false;
-    checkboxFive.checked = false;
-});
-labelTwo.addEventListener('click', ()=>{
-    checkboxOne.checked = false;
-    checkboxTwo.checked = true;
-    checkboxThree.checked = false;
-    checkboxFour.checked = false;
-    checkboxFive.checked = false;
-});
-labelThree.addEventListener('click', ()=>{
-    checkboxOne.checked = false;
-    checkboxTwo.checked = false;
-    checkboxThree.checked = true;
-    checkboxFour.checked = false;
-    checkboxFive.checked = false;
-});
-labelFour.addEventListener('click', ()=>{
-    checkboxOne.checked = false;
-    checkboxTwo.checked = false;
-    checkboxThree.checked = false;
-    checkboxFour.checked = true;
-    checkboxFive.checked = false;
-});
-labelFive.addEventListener('click', ()=>{
-    checkboxOne.checked = false;
-    checkboxTwo.checked = false;
-    checkboxThree.checked = false;
-    checkboxFour.checked = false;
-    checkboxFive.checked = true;
-});
-
-//window preload sets
-window.addEventListener('load', ()=>{
-    checkboxOne.checked = false;
-    checkboxTwo.checked = false;
-    checkboxThree.checked = true;
-    checkboxFour.checked = false;
-    checkboxFive.checked = false;
-});
-
 const imageUrls = [
     'png/john-wick.jpg',
+    'png/demon-slayer.png',
     'png/fastx.jpg',
-    'png/jagunjagun.jpg',
+    'png/spider-man.jpg',
     'png/sololeveling.jpg'
 ];
 
@@ -74,14 +13,40 @@ function changeBackgroundImage() {
     // Set the background image of .movie-banner
     movieBanner.style.backgroundImage = `url(${imageUrls[currentImageIndex]})`;
 
+    // Remove the active class from all checkboxes
+    checkboxes.forEach((checkbox) => {
+        checkbox.classList.remove('active-checkbox');
+    });
+
+    // Add the active class to the checkbox corresponding to the current image
+    checkboxes[currentImageIndex].classList.add('active-checkbox');
+
+    // Uncheck all checkboxes
+    checkboxes.forEach((checkbox) => {
+        checkbox.checked = false;
+    });
+
+    // Check the checkbox corresponding to the current image
+    checkboxes[currentImageIndex].checked = true;
+
     // Increment the image index or loop back to the first image
     currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
 }
 
-// Call the changeBackgroundImage function at specified intervals (e.g., every 5 seconds)
-const intervalTime = 8000; // Change image every 5 seconds (adjust as needed)
+// Call the changeBackgroundImage function at specified intervals (e.g., every 8 seconds)
+const intervalTime = 3000; // Change image every 8 seconds (adjust as needed)
 setInterval(changeBackgroundImage, intervalTime);
+
+// Add event listeners to checkboxes
+const checkboxes = document.querySelectorAll('.banner-carousel input[type="checkbox"]');
+checkboxes.forEach((checkbox, index) => {
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+            // Change the background image based on the checkbox index
+            changeBackgroundImage(index);
+        }
+    });
+});
 
 // Call the function once to set the initial background image
 changeBackgroundImage();
-
