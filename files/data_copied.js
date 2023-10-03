30d65c9564928ae6c451bf6d3808da09af3dbb57
limFakson <fakeyejoshua2005@gmail.com>
2023-10-03 10:22:10 +0100

    commit

--- files/data_copied.js
+++ files/data_copied.js
@@ -43,7 +43,7 @@ function toggleMenu() {
   });
 
   function handleSwipe() {
-    const swipeThreshold = 50; // Adjust this value as needed
+    const swipeThreshold = 60; // Adjust this value as needed
 
     if (touchStartX - touchEndX > swipeThreshold) {
       // Swipe to the left (closing menu)
